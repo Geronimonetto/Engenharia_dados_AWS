408dc07422810a9ad0540ed748b937f4f142020a
geronimo morais <geronimomorais@hotmail.com>
2023-07-20 15:48:38 -0300

    Enviando Desafios Udemy

--- Sprint_02/SQL/Exercícios/Desafios_Udemy/01.Resposta_Exercício_01.sql
+++
@@ -1,9 +0,0 @@
-/*Resposta 01*/
-SELECT DISTINCT city as cidades FROM sales.customers WHERE state='MG' ORDER BY city;
-
-/*Resposta 02*/
-SELECT visit_id FROM sales.funnel WHERE paid_date is not null ORDER BY paid_date DESC LIMIT 10;
-
-
-/*Resposta 03*/
-SELECT * FROM sales.customers WHERE birth_date >= '20000101' ORDER BY score DESC LIMIT 10;
