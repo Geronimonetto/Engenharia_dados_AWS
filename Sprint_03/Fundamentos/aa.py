cc1818a15225b98de9ac37b05d458dec8edd620f
geronimo morais <geronimomorais@hotmail.com>
2023-07-28 18:28:44 -0300

    Python - Enviando exercícios 021 & 022

--- Sprint_03/Fundamentos/aa.py
+++
@@ -1,8 +0,0 @@
-def nomes(*args,**kwargs):
-    for valor in kwargs.values():
-        return *args,valor
-
-valores= nomes(1, 3, 4, 'hello', parametro_nomeado='alguma coisa', x=20)
-
-for nome in valores:
-    print(nome)
