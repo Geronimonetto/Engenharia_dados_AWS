8bed984048f8116239f05a0cd081404a5c43c57f
geronimo morais <geronimomorais@hotmail.com>
2023-10-30 18:06:18 -0300

    SPRINT - 09- Finalizando Sprint

--- Sprint_08/Etapa_02/lambda_function_movies.py
+++ Sprint_08/Etapa_02/lambda_function_movies.py
@@ -2,42 +2,7 @@ import json
 import pandas as pd
 import boto3
 import requests
-from concurrent.futures import ThreadPoolExecutor
 
-
-def upload_to_s3(data: list, bucket_name: str, s3_key: str) -> None:
-    """
-    func: A função trata de enviar os arquivos diretamente para o bucket do S3 sem precisar armazenar em memória
-    temporária.
-
-    Parameters
-    ----------
-    data
-    bucket_name
-    s3_key
-
-    Returns
-    -------
-
-    """
-    # Setando as chaves de acesso para permitir acesso ao bucket S3
-    session = boto3.Session(
-        aws_access_key_id='AKIAZYAXJ7CO2LO2CJG2',
-        aws_secret_access_key='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'
-    )
-
-    # Indicando qual serviço queremos utilizar
-    s3_client = session.client('s3')
-
-    try:
-        # Convertendo os dados JSON em uma string
-        json_data = json.dumps(data, ensure_ascii=False)
-
-        # Enviando os dados JSON diretamente para o S3
-        s3_client.put_object(Bucket=bucket_name, Key=s3_key, Body=json_data)
-
-    except Exception as e:
-        print(f"Erro ao enviar os dados para o S3: {str(e)}")
 
 
 def lambda_handler(event, context):
@@ -52,69 +17,64 @@ def lambda_handler(event, context):
     -------
 
     """
-    session = boto3.Session(
+    s3 = boto3.client('s3',
         aws_access_key_id='AKIAZYAXJ7CO2LO2CJG2',
         aws_secret_access_key='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'
     )
-
-    s3_client = session.client('s3')
 
     bucket_name = 'data-lake-desafio-final'
     s3_file_name = 'Raw/Local/CSV/Movies/2023/09/26/movies.csv'
 
     try:
-        genero1 = 'Horror'
-        genero2 = 'Mystery'
-        # Setando o objeto do bucket S3 que queremos buscar
-        objeto = s3_client.get_object(Bucket=bucket_name, Key=s3_file_name)
+        # Gêneros desejados
+        generos = ['Horror', 'Mystery']
 
-        # Transformando o objeto setado do S3 em um dataframe
+        # Obter objeto do bucket S3
+        objeto = s3.get_object(Bucket=bucket_name, Key=s3_file_name)
+
+        # Ler arquivo CSV diretamente do objeto do S3 e criar DataFrame
         df = pd.read_csv(objeto['Body'], sep='|', na_values=[r'\N'], dtype={'NomeDaColuna': str})
 
-        # Dataframe com filmes de Horror
-        movies_horror = df[df['genero'].str.contains(genero1, case=True, na=False)]
+        # Filtrar filmes por gênero
+        all_movies = df[df['genero'].isin(generos)].drop_duplicates()
 
-        # Dataframe com filmes de misterio
-        movies_mystery = df[df['genero'].str.contains(genero2, case=True, na=False)]
-
-        # Unindo os 2 Dataframes
-        all_movies = pd.concat([movies_horror, movies_mystery])
-
-        # Removendo valores duplicados
-        all_movies_no_duplicates = all_movies.drop_duplicates()
-
-        # Buscando id dos filmes no Dataframe
-        lista_ids = all_movies_no_duplicates['id'].tolist()
-
-        # Convertendo a lista de id para set, para remover duplicados
-        conjunto_ids = set(lista_ids)
-
-        # Convertendo em lista para iterar
-        lista_ids_unicos = list(conjunto_ids)
+        # Obter IDs únicos dos filmes
+        lista_ids_unicos = all_movies['id'].unique().tolist()
 
         api_key = "ea33ae145ce37250f8ab09b9583b7a7f"
         dados = []  # Lista para adicionar os dados que vão para o JSON
         n_file = 1  # Número para ordenar o nome dos arquivos
 
         for id_movie in lista_ids_unicos:
-            url = f'https://api.themoviedb.org/3/movie/{id_movie}?api_key={api_key}'  # API
+            url = f'https://api.themoviedb.org/3/movie/{id_movie}?api_key={api_key}&language=en-US&append_to_response=credits'  # API
             response = requests.get(url)
             if response.status_code == 200:
                 data = response.json()
                 dados.append(data)
 
                 if len(dados) == 100:
+
+                    # Transformando a lista 'dados' em um objeto JSON
+
+                    json_file = json.dumps(dados, indent=4)
+                    json_clear = json_file[1:-1].replace("\n", "")
                     # Fazendo upload dos dados diretamente para o S3
-                    s3_key = f'Raw/TMDB/JSON/Movies/2023/10/13/Horror&Mystery/dados{n_file}.json'
-                    upload_to_s3(dados, bucket_name, s3_key)
+                    s3_key = f'Raw/TMDB/JSON/Movies/2023/10/13/HorrorMystery2/dados{n_file}.json'
+                    s3.put_object(Bucket = bucket_name,Key= s3_key,Body = json_clear )
                     n_file += 1
                     dados.clear()
-            else:
-                continue
-        # Fazendo upload de qualquer dado restante
+                    break
+                else:
+                    continue
+                # Fazendo upload de qualquer dado restante
         if dados:
-            s3_key = f'Raw/TMDB/JSON/Movies/2023/10/13/Horror&Mystery/dados{n_file}.json'
-            upload_to_s3(dados, bucket_name, s3_key)
+            json_file = json.dumps(dados, indent=4)
+            json_clear = json_file[1:-1].replace("\n", "")
+            # Fazendo upload dos dados diretamente para o S3
+            s3_key = f'Raw/TMDB/JSON/Movies/2023/10/13/HorrorMystery2/dados{n_file}.json'
+            s3.put_object(Bucket=bucket_name, Key=s3_key, Body=json_clear)
+            n_file += 1
+            dados.clear()
         else:
             print(f"Nenhum dado a ser enviado para o S3.")
 
